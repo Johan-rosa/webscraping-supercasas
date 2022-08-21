1ce392443e894a9500af1bc53c3040172298f919
Johan-rosa <johan.rosaperez@gmail.com>
2022-08-20 22:18:06 -0400

    global script for actions

---
+++ action.R
@@ -0,0 +1,6 @@
+# Restore dependecies -----------------------------------------------------
+install.packages("renv")
+renv::restore()
+
+# Sourcing file
+source("scripts/script_actualizacion_bd.R")
